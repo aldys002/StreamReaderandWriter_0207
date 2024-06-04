5149eac6d5a0e09152958207873a8d99df846aad
aldys002 <igidiaaldys@gmail.com>
2024-06-04 14:49:29 +0700

    mengubah tanda . menjadi _ pada kata infile is open

--- IoFile.cpp
+++ IoFile.cpp
@@ -15,6 +15,36 @@ int main() {
 
     // unlimited loop untuk menulis 
     while(true) {
-        
+        cout << "= ";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // Loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
     }
+    // selesai dalam menulis sekarang tutup file nya
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    // menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan dsini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai
+        infile.close();
+    }
+    // jika tidak di temukan file makan akan menampilkan ini
+    else cout << "unable to open file";
+    return 0;
 }
