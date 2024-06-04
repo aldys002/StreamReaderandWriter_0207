a596191a2824ddf181f51d8ea15069fc66ea0750
aldys002 <igidiaaldys@gmail.com>
2024-06-04 15:20:31 +0700

    membuat fungsi exception

--- manualexception.cpp
+++ manualexception.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main()
 {
-    
+    try {
+        cout << "Selamat Belajar di Prodi TI UMYEAH" << endl;
+        throw 0.5; // melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan di eksekusi" << endl;
+    }
+    catch (int a) {
+        // blok ini di eksekusi 
+        cout << "Pengecualian akan di eksekusi" << endl;
+    }
+    catch (...) {
+        /*jika selain integer maka block ini di eksekusi*/
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+    return 0;
 }
