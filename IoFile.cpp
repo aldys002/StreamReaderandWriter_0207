4c9aa46d319a094cc10f818f37d6cba87ab3bf1b
aldys002 <igidiaaldys@gmail.com>
2024-06-04 14:33:57 +0700

    membuat fungsi while

--- IoFile.cpp
+++ IoFile.cpp
@@ -4,5 +4,17 @@
 using namespace std;
 
 int main() {
-    
+    string baris;
+
+    // membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file 
+    outfile.open("contohfile.text");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    // unlimited loop untuk menulis 
+    while(true) {
+        
+    }
 }
