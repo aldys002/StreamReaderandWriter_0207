4e3b8a1e8268fc561e06d6dc25b49531551233c8
aldys002 <igidiaaldys@gmail.com>
2024-06-04 15:08:25 +0700

    menambah penanda 2

--- exceptionStandar.cpp
+++ exceptionStandar.cpp
@@ -6,4 +6,18 @@
 using namespace std;
 int main() {
     cout << "Awal Program" << endl; // penanda 1:...
+    try {
+        array<int, 3> data = { 3, 5, 7 };
+        //pesan array integer 3 elemen
+        cout << data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris Program Yang Terakhir" << endl;
+    /*penanda 2 : bahasa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    
 }
